4a4d078a4f94f202414eabc2a95fcaa89143d17e
chichumahesh <maheramachandran@gmail.com>
2020-02-26 05:38:30 -0500

    modified

--- src/main/java/com/mahesh/project/petclinicconfigserver/PetclinicconfigserverApplication.java
+++ src/main/java/com/mahesh/project/petclinicconfigserver/PetclinicconfigserverApplication.java
@@ -2,8 +2,10 @@ package com.mahesh.project.petclinicconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class PetclinicconfigserverApplication {
 
     public static void main(String[] args) {
