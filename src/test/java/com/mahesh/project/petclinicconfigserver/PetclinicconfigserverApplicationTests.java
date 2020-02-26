263e51e71a506a78dad8b7a44701773a62de8b0d
chichumahesh <maheramachandran@gmail.com>
2020-02-26 05:08:08 -0500

    commit petclinic config server

---
+++ src/test/java/com/mahesh/project/petclinicconfigserver/PetclinicconfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mahesh.project.petclinicconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetclinicconfigserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
